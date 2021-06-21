b45b26b7f77603254341a4d36c827582de829bf3
HOST <sh-kin@mail.ru>
2021-06-21 11:44:24 +0300

    Bottom navigation with Navigation component implemented

--- app/src/main/java/com/shishkin/itransition/gui/activities/MainActivity.kt
+++ app/src/main/java/com/shishkin/itransition/gui/activities/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.shishkin.itransition.gui.activities
 
 import android.os.Bundle
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.shishkin.itransition.R
 import dagger.android.support.DaggerAppCompatActivity
 
@@ -10,6 +13,10 @@ class MainActivity : DaggerAppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //        TODO viewBinding
+        val navController = findNavController(this, R.id.host_fragment)
+        val bottomNavigationView : BottomNavigationView = findViewById(R.id.bottom_navigation_view)
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
     }
 }
 
