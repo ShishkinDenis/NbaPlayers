087386eeec94c2b77feed747ae4985915de49647
Evgeny Fetskovich <evgeny.fetskovich@betbull.com>
2021-07-07 10:22:31 +0300

    Add test FULL commit

--- app/src/main/java/com/shishkin/itransition/repository/DefaultNbaRepository.kt
+++ app/src/main/java/com/shishkin/itransition/repository/DefaultNbaRepository.kt
@@ -15,6 +15,8 @@ import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
+// My first test commit
+// My second test commit
 class DefaultNbaRepository @Inject constructor(
     private val nbaPlayerDao: NbaPlayerDao,
     private val nbaApi: NbaApi?
