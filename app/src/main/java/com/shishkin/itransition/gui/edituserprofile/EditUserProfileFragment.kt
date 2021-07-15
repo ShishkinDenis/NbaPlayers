3e12721a2f16cb684ae6000d66c41db11e35b14a
HOST <sh-kin@mail.ru>
2021-07-15 12:40:42 +0300

    DatePickerDialog created

--- app/src/main/java/com/shishkin/itransition/gui/edituserprofile/EditUserProfileFragment.kt
+++ app/src/main/java/com/shishkin/itransition/gui/edituserprofile/EditUserProfileFragment.kt
@@ -1,14 +1,21 @@
 package com.shishkin.itransition.gui.edituserprofile
 
+import android.app.DatePickerDialog
+import android.icu.util.Calendar
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.viewModels
+import com.shishkin.itransition.R
 import com.shishkin.itransition.databinding.FragmentEditUserProfileBinding
 import dagger.android.support.DaggerFragment
+import java.text.SimpleDateFormat
+import java.util.*
 import javax.inject.Inject
 
+private const val USER_BIRTH_DATE_FORMAT = "dd/MM/yyyy"
 
 class EditUserProfileFragment : DaggerFragment() {
 
@@ -18,6 +25,7 @@ class EditUserProfileFragment : DaggerFragment() {
 
     private lateinit var _binding: FragmentEditUserProfileBinding
     private val binding get() = _binding
+    private val calendar = Calendar.getInstance()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,5 +37,48 @@ class EditUserProfileFragment : DaggerFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.etEditUserProfileDateChooser.setOnClickListener {
+            showDatePickerDialog()
+        }
+    }
+
+    private fun updateDateInDateChooser() {
+        val sdf = SimpleDateFormat(USER_BIRTH_DATE_FORMAT, Locale.US)
+        val chosenDate = calendar.time
+        val currentDate = Date()
+        val chosenConvertedDate = sdf.format(chosenDate)
+
+        if (chosenDate.before(currentDate)) {
+            binding.etEditUserProfileDateChooser.setText(chosenConvertedDate)
+        } else {
+            Toast.makeText(
+                context,
+                getString(R.string.edit_user_profile_not_valid_date_toast_message),
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
+    private fun setDateListener(): DatePickerDialog.OnDateSetListener {
+        return DatePickerDialog.OnDateSetListener { _, year, monthOfYear, dayOfMonth ->
+            with(calendar) {
+                set(Calendar.YEAR, year)
+                set(Calendar.MONTH, monthOfYear)
+                set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            }
+            updateDateInDateChooser()
+        }
+    }
+
+    private fun showDatePickerDialog() {
+        context?.let { context ->
+            DatePickerDialog(
+                context, setDateListener(),
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH)
+            ).show()
+        }
     }
 }
---
+++ app/src/main/java/com/shishkin/itransition/gui/edituserprofile/UserUi.kt
@@ -0,0 +1,6 @@
+package com.shishkin.itransition.gui.edituserprofile
+
+data class UserUi(
+    val name: String,
+    val birthDate: String
+)
