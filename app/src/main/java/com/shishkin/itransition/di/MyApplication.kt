6c583cbd752964f5c24ea245b8bf31a891153f6f
HOST <sh-kin@mail.ru>
2021-07-28 12:52:10 +0300

    TestWorker deleted

--- app/src/main/java/com/shishkin/itransition/di/MyApplication.kt
+++ app/src/main/java/com/shishkin/itransition/di/MyApplication.kt
@@ -3,7 +3,6 @@ package com.shishkin.itransition.di
 import androidx.work.Configuration
 import androidx.work.DelegatingWorkerFactory
 import com.shishkin.itransition.workers.ImageCompressionWorkerFactory
-import com.shishkin.itransition.workers.TestWorkerFactory
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
 import timber.log.Timber
@@ -13,18 +12,15 @@ import javax.inject.Inject
 
 class MyApplication : DaggerApplication(), Configuration.Provider {
 
+    @Inject
+    lateinit var imageCompressionWorkerFactory: ImageCompressionWorkerFactory
+
     override fun onCreate() {
         super.onCreate()
         if (com.shishkin.itransition.BuildConfig.DEBUG) {
             Timber.plant(DebugTree())
         }
     }
-
-    @Inject
-    lateinit var imageCompressionWorkerFactory: ImageCompressionWorkerFactory
-
-    @Inject
-    lateinit var testWorkerFactory: TestWorkerFactory
 
     override fun applicationInjector(): AndroidInjector<MyApplication> =
         DaggerAppComponent
@@ -34,7 +30,6 @@ class MyApplication : DaggerApplication(), Configuration.Provider {
     override fun getWorkManagerConfiguration(): Configuration {
         val delegatingWorkerFactory = DelegatingWorkerFactory()
         delegatingWorkerFactory.addFactory(imageCompressionWorkerFactory)
-        delegatingWorkerFactory.addFactory(testWorkerFactory)
 
         return Configuration.Builder()
             .setMinimumLoggingLevel(android.util.Log.DEBUG)
